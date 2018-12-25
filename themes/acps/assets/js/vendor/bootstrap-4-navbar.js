cbaa8d8781f4e49f3815d34a84543c18baef4ecb
Jimmy Harris <jimmyharris+git@protonmail.com>
2018-12-25 16:34:28 +1100

    Integrate multi-dropdown menu CSS and JS.  Fixes #6.

--- themes/acps/assets/js/vendor/bootstrap-4-navbar.js
+++ themes/acps/assets/js/vendor/bootstrap-4-navbar.js
@@ -1,11 +1,3 @@
-
-/*!
- * Bootstrap 4 multi dropdown navbar ( https://bootstrapthemes.co/demo/resource/bootstrap-4-multi-dropdown-navbar/ )
- * Copyright 2017.
- * Licensed under the GPL license
- */
-
-
 $( document ).ready( function () {
     $( '.dropdown-menu a.dropdown-toggle' ).on( 'click', function ( e ) {
         var $el = $( this );
@@ -15,13 +7,13 @@ $( document ).ready( function () {
         }
         var $subMenu = $( this ).next( ".dropdown-menu" );
         $subMenu.toggleClass( 'show' );
-        
+
         $( this ).parent( "li" ).toggleClass( 'show' );
 
         $( this ).parents( 'li.nav-item.dropdown.show' ).on( 'hidden.bs.dropdown', function ( e ) {
             $( '.dropdown-menu .show' ).removeClass( "show" );
         } );
-        
+
          if ( !$parent.parent().hasClass( 'navbar-nav' ) ) {
             $el.next().css( { "top": $el[0].offsetTop, "left": $parent.outerWidth() - 4 } );
         }
